7fff3eb729261c9bd5bb673a8b2ad505f3a12ffb
gin7018 <79063763+gin7018@users.noreply.github.com>
2023-03-04 12:34:56 -0500

    initial commit

---
+++ wacknpack/src/main/java/com/app/wacknpack/WacknpackApplication.java
@@ -0,0 +1,13 @@
+package com.app.wacknpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WacknpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WacknpackApplication.class, args);
+	}
+
+}
---
+++ wacknpack/src/test/java/com/app/wacknpack/WacknpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wacknpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WacknpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
