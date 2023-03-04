9307c7b8d0916d00b722344d23ff9f28101b6044
Hana Ho <hth2539@rit.edu>
2023-03-04 15:21:03 -0500

    got rid of final

--- wacknpack/src/main/java/com/app/wacknpack/model/Location.java
+++ wacknpack/src/main/java/com/app/wacknpack/model/Location.java
@@ -16,7 +16,6 @@ public class Location {
     @JsonProperty("donationType")
     private DonationAccepted donationType;
 
-
     public Location (@JsonProperty("locationId") int locationId, @JsonProperty("locationName") String locationName,
                      @JsonProperty("address") String address, @JsonProperty("longitude") Long longitude,
                      @JsonProperty("latitude") Long latitude, @JsonProperty("donationType") DonationAccepted donationType) {
--- wacknpack/src/main/java/com/app/wacknpack/model/Rewards.java
+++ wacknpack/src/main/java/com/app/wacknpack/model/Rewards.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Rewards {
     @JsonProperty("userId")
-    private final int userId;
+    private int userId;
     @JsonProperty("points")
-    private final int points;
+    private int points;
 
     public Rewards (@JsonProperty("userId") int userId, @JsonProperty("points") int points) {
         this.userId = userId;
