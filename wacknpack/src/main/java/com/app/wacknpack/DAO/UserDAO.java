dd119e2dcb6d62c404856176f15479b93aa04fcb
Hana Ho <hth2539@rit.edu>
2023-03-04 17:53:33 -0500

    code clean up

--- wacknpack/src/main/java/com/app/wacknpack/DAO/UserDAO.java
+++ wacknpack/src/main/java/com/app/wacknpack/DAO/UserDAO.java
@@ -5,47 +5,15 @@ import java.io.IOException;
 import com.app.wacknpack.model.User;
 
 public interface UserDAO {
-    /**
-     * get all the Users in the file
-     * 
-     * @return list of all the users in the file
-     * @throws IOException
-     */
+
     User[] getUsers() throws IOException;
 
-    /**
-     * Return a list of {@link User users} related to a search query
-     * 
-     * @param query
-     * @return array of users object
-     * @throws IOException
-     */
     User[] findUsers(String query) throws IOException;
 
-    /**
-     * Returns selected user based off of ID
-     * 
-     * @param id
-     * @return singular user object
-     * @throws IOException
-     */
     User getUser(int id) throws IOException;
 
-    /**
-     * Create a new {@link User user} account given the required informqtion
-     * 
-     * @param User object
-     * @return the new {@link User users} if successful, null otherwise
-     * @throws IOException
-     */
     User registerUser(User user) throws IOException;
 
-    /**
-     * Checks whether a user has the correct credentials to log in
-     * @param user the user that want to login in
-     * @return true if valid user, false otherwise
-     * @throws IOException
-     */
     User loginUser(String username, String password) throws IOException;
 
 }
--- wacknpack/src/main/java/com/app/wacknpack/persistent/UserFileDAO.java
+++ wacknpack/src/main/java/com/app/wacknpack/persistent/UserFileDAO.java
@@ -2,24 +2,19 @@ package com.app.wacknpack.persistent;
 
 import java.io.IOException;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
-
 import com.app.wacknpack.DAO.UserDAO;
 import com.app.wacknpack.model.User;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 
 public class UserFileDAO implements UserDAO{
     public Map<Integer, User> users; // Provides a local cache of the objects
     private ObjectMapper objectMapper; // Provides conversion between objects and JSON text format written to the file
-    private static int nextId; // The next Id to assign to a new painting
+    private static int nextId; // The next id to assign
     private String filename; // Filename to read from and write to
 
     /**
@@ -34,7 +29,7 @@ public class UserFileDAO implements UserDAO{
     public UserFileDAO(@Value("${users.file}") String filename, ObjectMapper objectMapper) throws IOException {
         this.filename = filename;
         this.objectMapper = objectMapper;
-        load(); // load the paintings from the file
+        load();
     }
 
     /**
@@ -54,7 +49,7 @@ public class UserFileDAO implements UserDAO{
      * @return The array of {@link User users}, may be empty
      */
     private User[] getUsersArray() {
-        return getUserssArray(null);
+        return getUsersArray(null);
     }
 
     /**
@@ -66,10 +61,10 @@ public class UserFileDAO implements UserDAO{
      * If containsText is null, the array contains all of the {@linkplain User
      * users}
      * in the tree map
-     * 
+     *
      * @return The array of {@link User users}, may be empty
      */
-    private User[] getUserssArray(String containsText) { // if containsText == null, no filter
+    private User[] getUsersArray(String containsText) { // if containsText == null, no filter
         ArrayList<User> userlst = new ArrayList<>();
 
         for (User user : users.values()) {
@@ -86,9 +81,9 @@ public class UserFileDAO implements UserDAO{
     /**
      * Saves the {@linkplain User users} from the map into the file as an
      * array of JSON objects
-     * 
+     *
      * @return true if the {@link User users} were written successfully
-     * 
+     *
      * @throws IOException when file cannot be accessed or written to
      */
     private boolean save() throws IOException {
@@ -105,27 +100,25 @@ public class UserFileDAO implements UserDAO{
      * Loads {@linkplain User users} from the JSON file into the map
      * <br>
      * Also sets next id to one more than the greatest id found in the file
-     * 
+     *
      * @return true if the file was read successfully
-     * 
+     *
      * @throws IOException when file cannot be accessed or read from
      */
     private boolean load() throws IOException {
         users = new TreeMap<>();
         nextId = 0;
 
-        // Deserializes the JSON objects from the file into an array of heroes
+        // Deserializes the JSON objects from the file into an array of users
         // readValue will throw an IOException if there's an issue with the file
         // or reading from the file
         User[] user_arr = objectMapper.readValue(new File(filename), User[].class);
 
-        // Add each hero to the tree map and keep track of the greatest id
         for (User user : user_arr) {
             users.put(user.getUserId(), user);
             if (user.getUserId() > nextId)
                 nextId = user.getUserId();
         }
-        // Make the next id one greater than the maximum from the file
         ++nextId;
         return true;
     }
@@ -138,7 +131,7 @@ public class UserFileDAO implements UserDAO{
 
     @Override
     public User[] findUsers(String query) throws IOException {
-        return getUserssArray(query);
+        return getUsersArray(query);
     }
 
     @Override
@@ -169,6 +162,4 @@ public class UserFileDAO implements UserDAO{
         }
         return null;
     }
-
-    
 }
