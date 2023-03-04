dca2ad93d6d46c25dc1faffd70a9ecbe9a2c1b06
Hana Ho <hth2539@rit.edu>
2023-03-04 13:40:16 -0500

    rewards created

--- wacknpack/src/main/java/com/app/wacknpack/model/Rewards.java
+++ wacknpack/src/main/java/com/app/wacknpack/model/Rewards.java
@@ -3,5 +3,17 @@ package com.app.wacknpack.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Rewards {
-    
+    @JsonProperty("userId")
+    private final int userId;
+    @JsonProperty("points")
+    private final int points;
+
+    public Rewards (@JsonProperty("userId") int userId, @JsonProperty("points") int points) {
+        this.userId = userId;
+        this.points = points;
+    }
+
+    public int getUserId() {return userId;}
+
+    public int getPoints() {return points;}
 }
