4397187b3bd3e21c751fdfb6eba47bab83390b81
Claudia Snyder <cws8272@rit.edu>
2023-03-04 13:25:20 -0500

    added json properties to user model

--- wacknpack/src/main/java/com/app/wacknpack/model/User.java
+++ wacknpack/src/main/java/com/app/wacknpack/model/User.java
@@ -1,18 +1,24 @@
 package com.app.wacknpack.model;
 
 public class User {
+    @JsonProperty("user_id")
+    private final int user_id;
 
-    private final int id;
+    @JsonProperty("email")
     private final String email;
+
+    @JsonProperty("password")
     private final String password;
-    private final Type userType;
+
+    @JsonProperty("user_type")
+    private final Type user_type;
 
 
-    public User (int id, String email, String password, Type userType) {
-        this.id = id;
+    public User (@JsonProperty("uesr_id") int user_id, @JsonProperty("email") String email, @JsonProperty("password") String password, @JsonProperty("user_type") Type user_type) {
+        this.user_id = user_id;
         this.email = email;
         this.password = password;
-        this.userType = userType;
+        this.user_type = user_type;
     }
 
     public enum Type {
