6b94215f05348361d6936fb129869cc8b6508cc8
gin7018 <79063763+gin7018@users.noreply.github.com>
2023-03-04 22:02:12 -0500

    adding categories to module declaration

--- wackui/src/app/app.module.ts
+++ wackui/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, NavigationbarComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavigationbarComponent,
+    CategoriesComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
